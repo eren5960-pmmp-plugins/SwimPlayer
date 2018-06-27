acb418ecd57e28c433d6fd07834fee8e8476e114
Eren Ahmed Akyol <ahmederen123@gmail.com>
2018-06-27 22:25:10 +0300

    First commits

---
+++ src/Eren5960/SwimPlayer/Swim.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Eren5960\SwimPlayer;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerMoveEvent;
+
+class Swim extends PluginBase implements Listener{
+	
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this,$this);
+	}
+	public function yuzz(PlayerMoveEvent $e){
+		$o = $e->getPlayer();
+		if($e->getFrom()->getSide(0)->getId() == 9 && $e->getFrom()->getSide(0)->getId() == 8){
+			if($o->hasPermission("swim")){
+			$o->setSwimming(true);
+			}
+		}
+	}
+} 
