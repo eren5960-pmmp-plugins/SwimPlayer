e586e0b5b504f1ca80e5f9e5cc10b4610f9f64ec
Eren Ahmed Akyol <ahmederen123@gmail.com>
2018-08-19 10:56:49 +0300

    Optimize & CleanUp

--- src/Eren5960/SwimPlayer/Swim.php
+++ src/Eren5960/SwimPlayer/Swim.php
@@ -1,22 +1,34 @@
 <?php
-
+/**
+* _____ ____ ___ __ ___ 
+* | ____| _ __ ___ _ __ | ___| / _ \ / /_ / _ \ 
+* | _| | '__|/ _ \| '_ \ |___ \| (_) || '_ \ | | | |
+* | |___ | | | __/| | | | ___) |\__, || (_) || |_| |
+* |_____||_| \___||_| |_||____/ /_/ \___/ \___/ 
+* 
+* @version v1
+* @name JobForm
+* @author Eren5960
+* @link https://github.com/Eren5960/JobForm
+*/
 namespace Eren5960\SwimPlayer;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\event\{
+    Listener, \player\PlayerMoveEvent
+};
 
 class Swim extends PluginBase implements Listener{
 	
-	public function onEnable(){
-		$this->getServer()->getPluginManager()->registerEvents($this,$this);
+	public function onEnable(): void{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
-	public function yuzz(PlayerMoveEvent $e){
-		$o = $e->getPlayer();
-		if($o->getLevel()->getBlock($o->getSide(0))->getId() == 9 || $o->getLevel()->getBlock($o->getSide(0))->getId() == 8){
-			if($o->hasPermission("use.swim")){
-			$o->setSwimming(true);
-			}
+	public function onMove(PlayerMoveEvent $e){
+		$player = $e->getPlayer();
+      if(!$player->hasPermission("use.swim")) return;
+
+		if($player->isUnderWater()){
+			    $player->setSwimming(true);
 		}
 	}
 } 
